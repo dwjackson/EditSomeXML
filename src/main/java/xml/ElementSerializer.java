6aaf984e35bc76fd3086f7fab04b869356d803b9
David Jackson <david@dwjackson.ca>
2014-10-04 16:55:55 -0400

    Create Element Serializer interface

---
+++ src/main/java/xml/ElementSerializer.java
@@ -0,0 +1,24 @@
+package xml;
+
+/**
+ * Classes that implement the ElementSerializer format are used to transform
+ * Elements into formats that can be stored and transmitted.
+ *
+ * @see Element
+ */
+public interface ElementSerializer {
+    /**
+     * Serialize the element tree to a string
+     * @param root The root element of the tree
+     * @return A string representation of the element
+     */
+    String serializeToString(Element root);
+
+    /**
+     * Serialize the element tree to a file using the same format as that
+     * which is created by the serializeToString() method.
+     * @param root The root of the element tree
+     * @param fileName The name of the file into which to serialize the file
+     */
+    void serializeToFile(Element root, String fileName);
+}
---
+++ src/main/java/xml/ElementXMLSerializer.java
@@ -0,0 +1,18 @@
+package xml;
+
+/**
+ * The ElementXMLSerializer is used to serialize an Element into the usual XML
+ * format.
+ *
+ * @see xml.ElementSerializer
+ * @see xml.Element
+ */
+public class ElementXMLSerializer implements ElementSerializer {
+    public String serializeToString(Element root) {
+        return null;
+    }
+
+    public void serializeToFile(Element root, String fileName) {
+
+    }
+}
