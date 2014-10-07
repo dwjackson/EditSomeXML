3e1860fea9045372db175b06ecd4cca3c280cb25
David Jackson <david@dwjackson.ca>
2014-10-06 21:51:38 -0400

    Add explanatory JavaDoc

--- src/main/java/editor/ElementTreeView.java
+++ src/main/java/editor/ElementTreeView.java
@@ -6,12 +6,18 @@ import xml.Element;
 import javax.swing.*;
 
 /**
- * Created by davidjackson on 2014-10-06.
+ * The ElementTreeView controls the section of the GUI that displays the tree
+ * representation of an XML element tree.
  */
 public class ElementTreeView extends JPanel implements Observer {
     private JTree tree;
     private Element root;
 
+    /**
+     * Create the tree view and associate it with a particular model (root of
+     * the XML element tree)
+     * @param root The root element of the XML element tree.
+     */
     public ElementTreeView(Element root) {
         root.registerObserver(this);
         this.root = root;
@@ -22,6 +28,9 @@ public class ElementTreeView extends JPanel implements Observer {
         setVisible(false);
     }
 
+    /**
+     * When the associated element tree root has changed, change the view
+     */
     public void notifyObserver() {
         if (root != null) {
             // TODO: Change the tree
