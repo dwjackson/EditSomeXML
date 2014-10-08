7664d2cfca747a11b43b08d3a406e9abcb98aee2
David Jackson <david@dwjackson.ca>
2014-10-08 19:50:46 -0400

    Add accessor methods to Element

--- src/main/java/xml/Element.java
+++ src/main/java/xml/Element.java
@@ -5,6 +5,7 @@ import utility.Observable;
 import utility.Observer;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 /**
@@ -21,6 +22,7 @@ public class Element implements Observable {
     private HashMap<String, String> attributes;
     private ArrayList<Element> children;
     private GenericObservable observable;
+    private Element parent;
 
     /**
      * Initialize an Element with no data in it. This element is invalid until
@@ -32,6 +34,7 @@ public class Element implements Observable {
         attributes = new HashMap<String, String>();
         children = new ArrayList<Element>();
         observable = new GenericObservable();
+        parent = null;
     }
 
     /**
@@ -44,6 +47,7 @@ public class Element implements Observable {
         attributes = new HashMap<String, String>();
         children = new ArrayList<Element>();
         observable = new GenericObservable();
+        parent = null;
     }
 
     /**
@@ -82,5 +86,68 @@ public class Element implements Observable {
      */
     public void notifyObservers() {
         observable.notifyObservers();
+    }
+
+    /**
+     * Return this element's children as a collection
+     * @see java.util.Collection
+     * @return This element's children, as a Collection
+     */
+    public Collection<Element> getChildren() {
+        return children;
+    }
+
+    /**
+     * Determine if this element has children
+     * @return true if this element has childre, false if it doesn't
+     */
+    public boolean hasChildren() {
+        if (children.size() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the child at a particular index. Child elements have an order:
+     * those added earlier will precede those added later.
+     * @param index The index of the child
+     * @return The child Element if it's found, null if it isn't
+     */
+    public Element getChildAt(int index) {
+        Element child = null;
+        if (index >= 0 && index < children.size()) {
+            child = children.get(index);
+        }
+        return child;
+    }
+
+    /**
+     * Determine the number of children that this Element has
+     * @return the number of children that this Element has
+     */
+    public int getNumberOfChildren() {
+        return children.size();
+    }
+
+    /**
+     * Get the index of a known child
+     * @param elem The child element to look for
+     * @return the index of the child if it's found, -1 if not
+     */
+    public int getChildIndex(Element elem) {
+        int index = -1;
+        if (children.contains(elem)) {
+            index = children.indexOf(elem);
+        }
+        return index;
+    }
+
+    /**
+     * Get this Element's parent element, if it exists
+     * @return This Element's parent or null if it doesn't have one
+     */
+    public Element getParent() {
+        return parent;
     }
 }
