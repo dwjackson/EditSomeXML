6d6137b868986d3a8a9a45ec5fc50e7aa390cb3e
David Jackson <david@dwjackson.ca>
2014-10-13 23:00:49 -0400

    Make number of attributes in editor variable

--- src/main/java/editor/views/AttributesPanelView.java
+++ src/main/java/editor/views/AttributesPanelView.java
@@ -2,6 +2,7 @@ package editor.views;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -11,18 +12,36 @@ import java.util.HashMap;
  */
 public class AttributesPanelView extends JPanel {
     private JPanel attributesPanel;
+    private ArrayList<JTextField> attributeNameFields;
+    private ArrayList<JTextField> attributeValueFields;
 
     /**
      * Initialize the AttributesPanelView with no data
      */
     public AttributesPanelView() {
+        attributeNameFields = new ArrayList<JTextField>();
+        attributeValueFields = new ArrayList<JTextField>();
+
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         add(new JLabel("Attributes"));
 
         attributesPanel = new JPanel();
         attributesPanel.setLayout(new GridLayout());
-        attributesPanel.add(new JTextField("", 10));
-        attributesPanel.add(new JTextField("", 10));
+        addAttribute();
         add(attributesPanel);
+    }
+
+    /**
+     * Add more space for another attribute to be added. This adds a field for
+     * the attribute's name and another for its value.
+     */
+    public void addAttribute() {
+        JTextField nameField = new JTextField("", 10);
+        attributesPanel.add(nameField);
+        attributeNameFields.add(nameField);
+
+        JTextField valueField = new JTextField("", 10);
+        attributesPanel.add(valueField);
+        attributeValueFields.add(valueField);
     }
 }
