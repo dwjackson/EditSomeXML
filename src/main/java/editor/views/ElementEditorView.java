7f4a0054609ad6fea437b2ebc09c43ca9eb8830b
David Jackson <david@dwjackson.ca>
2014-10-21 22:24:56 -0400

    Group up the document listeners

--- src/main/java/editor/views/ElementEditorView.java
+++ src/main/java/editor/views/ElementEditorView.java
@@ -1,11 +1,13 @@
 package editor.views;
 
+import editor.ElementDocumentListener;
 import editor.TagDocumentListener;
 import editor.TextDocumentListener;
 import xml.Element;
 
 import javax.swing.*;
 import javax.swing.text.Document;
+import java.util.ArrayList;
 
 /**
  * The ElementEditorView is used to edit the currently-selected element.
@@ -15,14 +17,14 @@ public class ElementEditorView extends JPanel {
     private AttributesPanelView attributesPanel;
     private JTextArea elementTextArea;
     private Element elem;
-    private TagDocumentListener tagDocumentListener;
-    private TextDocumentListener textDocumentListener;
+    private ArrayList<ElementDocumentListener> documentListeners;
 
     /**
      * Create the ElementEditorView and leave all of its fields blank
      */
     public ElementEditorView() {
         elem = null;
+        documentListeners = new ArrayList<ElementDocumentListener>();
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
@@ -30,7 +32,9 @@ public class ElementEditorView extends JPanel {
         JPanel tagPanel = new JPanel();
         tagPanel.add(new JLabel("Tag"));
         tagField = new JTextField("", 20);
+        TagDocumentListener tagDocumentListener;
         tagDocumentListener = new TagDocumentListener(elem);
+        documentListeners.add(tagDocumentListener);
         tagField.getDocument().addDocumentListener(tagDocumentListener);
         tagPanel.add(tagField);
         add(tagPanel);
@@ -46,7 +50,9 @@ public class ElementEditorView extends JPanel {
         elementTextArea.setRows(4);
         elementTextArea.setColumns(40);
         textPanel.add(elementTextArea);
+        TextDocumentListener textDocumentListener;
         textDocumentListener = new TextDocumentListener(elem);
+        documentListeners.add(textDocumentListener);
         Document doc = elementTextArea.getDocument();
         doc.addDocumentListener(textDocumentListener);
         add(textPanel);
@@ -60,8 +66,9 @@ public class ElementEditorView extends JPanel {
      */
     public void populateWithElementData(Element element) {
         elem = element;
-        tagDocumentListener.setElement(elem);
-        textDocumentListener.setElement(elem);
+        for (ElementDocumentListener listener : documentListeners) {
+            listener.setElement(elem);
+        }
         tagField.setText(elem.getTag());
         // TODO: Set the attributes
         elementTextArea.setText(elem.getText());
