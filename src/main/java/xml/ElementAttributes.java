c7a1f84d4a835e6c14efece9648ebb75727d8508
David Jackson <david@dwjackson.ca>
2014-10-24 22:26:02 -0400

    Add JavaDoc to ElementAttributes

--- src/main/java/xml/ElementAttributes.java
+++ src/main/java/xml/ElementAttributes.java
@@ -12,15 +12,27 @@ public class ElementAttributes {
     private HashMap<String, String> attributes;
     private ArrayList<String> attributeNames;
 
+    /**
+     * Initialize the attributes as an empty list
+     */
     public ElementAttributes() {
         attributes = new HashMap<String, String>();
         attributeNames = new ArrayList<String>();
     }
 
+    /**
+     * Get an iterable to go through the names of the attributes
+     * @return An Iterable for the attribute names
+     */
     public Iterable<String> names() {
         return attributeNames;
     }
 
+    /**
+     * Get the value of an attribute
+     * @param key The attribute's name
+     * @return The attribute's value, null if it doesn't exist
+     */
     public String getValue(String key) {
         if (attributeNames.contains(key)) {
             return attributes.get(key);
@@ -28,6 +40,11 @@ public class ElementAttributes {
         return null;
     }
 
+    /**
+     * Set the value of an attribute
+     * @param key The attribute's name
+     * @param value The attribute's value
+     */
     public void set(String key, String value) {
         attributes.put(key, value);
         if (!attributes.containsKey(key)) {
@@ -35,10 +52,18 @@ public class ElementAttributes {
         }
     }
 
+    /**
+     * Remove an attribute from the collection
+     * @param key The attribute's name
+     */
     public void remove(String key) {
         attributes.remove(key);
     }
 
+    /**
+     * Get the number of attributes
+     * @return the number of attributes
+     */
     public int size() {
         return attributeNames.size();
     }
