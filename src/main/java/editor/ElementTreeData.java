20f7cf34ffb07e226002a7f16cebc185faae3ab2
David Jackson <david@dwjackson.ca>
2014-10-27 22:05:10 -0400

    Add documentation to ElementTreeData class

--- src/main/java/editor/ElementTreeData.java
+++ src/main/java/editor/ElementTreeData.java
@@ -27,27 +27,44 @@ import xml.Element;
 import java.util.ArrayList;
 
 /**
- * The ElementTreeData class is used to encapsulate a tree of Elements
+ * The ElementTreeData class is used to encapsulate a tree of Elements. It
+ * contains the tree and observers of the tree. Primarily this class is used
+ * to enable the ElementTreeModel to access the tree.
  */
 public class ElementTreeData implements Observable {
     private Element root;
     private ArrayList<Observer> observers;
 
+    /**
+     * Initialize the tree data with no root and no observers
+     */
     public ElementTreeData() {
-        root = null;
-        observers = new ArrayList<Observer>();
+        this(null);
     }
 
+    /**
+     * Initialize the tree data with a particular root element
+     * @param elem The root element of the tree
+     */
     public ElementTreeData(Element elem) {
         root = elem;
         observers = new ArrayList<Observer>();
     }
 
+    /**
+     * Generate a new root element for the tree
+     * @return the new root element
+     */
     public Element newRoot() {
         root = new Element();
         return root;
     }
 
+    /**
+     * Set the root of the tree to a particular element
+     * @param elem the root element of the tree
+     * @return the root element of the tree
+     */
     public Element setRoot(Element elem) {
         root = elem;
         for (Observer observer : observers) {
@@ -56,24 +73,37 @@ public class ElementTreeData implements Observable {
         return root;
     }
 
+    /**
+     * Get the root element of the tree
+     * @return the root element of the tree
+     */
     public Element getRoot() {
         return root;
     }
 
+    /**
+     * @see utility.Observable
+     */
     @Override
     public void registerObserver(Observer observer) {
         observers.add(observer);
         root.registerObserver(observer);
     }
 
+    /**
+     * @see utility.Observable
+     */
     @Override
     public void unregisterObserver(Observer observer) {
         observers.remove(observer);
         root.unregisterObserver(observer);
     }
 
+    /**
+     * @see utility.Observable
+     */
     @Override
     public void notifyObservers(Object obj) {
-        root.notifyObservers(null);
+        root.notifyObservers(obj);
     }
 }
