e2234e0a0b04071bab342aef52699f48db8aa921
David Jackson <david@dwjackson.ca>
2014-11-08 13:42:28 -0500

    Add ElementCommand base class

--- src/main/java/xml/commands/ChangeTagCommand.java
+++ src/main/java/xml/commands/ChangeTagCommand.java
@@ -3,13 +3,12 @@ package xml.commands;
 import utility.Command;
 import xml.Element;
 
-public class ChangeTagCommand implements Command {
-	private Element element;
+public class ChangeTagCommand extends ElementCommand implements Command {
 	private String tag;
 	private String oldTag;
 	
 	public ChangeTagCommand(Element element, String tag) {
-		this.element = element;
+		super(element);
 		this.tag = tag;
 		oldTag = new String(element.getTag());
 	}
---
+++ src/main/java/xml/commands/ElementCommand.java
@@ -0,0 +1,10 @@
+package xml.commands;
+
+import xml.Element;
+
+public class ElementCommand {
+	protected Element element;
+	public ElementCommand(Element element) {
+		this.element = element;
+	}
+}
